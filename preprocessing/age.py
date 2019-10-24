904e63b71f23a6b7a69361f6451b6aa6c03b5db9
岸本陽大 <youodf11khp@gmail.com>
2019-10-24 15:51:08 +0900

    Complete pp on categorical features except access

---
+++ preprocessing/age.py
@@ -0,0 +1,17 @@
+# age : to float value 
+def pp_age(df):
+  age = df['age']
+  
+  age = age.map(lambda x: '0年' if x == '新築' else x)
+  
+  y_m = age.str.split('年', expand=True)
+  year = y_m.iloc[:, 0].astype(int)
+  month = y_m.iloc[:, 1]\
+    .str.replace("ヶ月", "")\
+    .map(lambda x: "0" if x == "" else x)\
+    .astype(int)
+  
+  df.loc[:, 'age'] = year + month / 12
+  
+  df.loc[df['age'] > 70, 'age'] = 70
+  return df
---
+++ preprocessing/contract.py
@@ -0,0 +1,15 @@
+# contract : to contract, periodic
+def pp_contract(df):
+  contract = df['contract'].str.replace("※この物件は\t定期借家\tです。", '').str.replace('\t', '')
+  contract[contract.str.contains('まで').fillna(True)] = '2年間'
+  year = contract.str.extract(r'(\d)年間').fillna(0).astype(int)
+  month = contract.str.extract(r'(\d)ヶ月間').fillna(0).astype(int)
+  contract = year + (month / 12)
+  contract = contract.iloc[:, 0]
+  
+  periodic = df['contract'].str.contains('定期借家').fillna(0).astype(int)
+  
+  df.loc[:, 'contract'] = contract
+  df.loc[:, 'periodic'] = periodic
+  
+  return df
