243cf8390f4e4ad3fc6f1e5efe188bde1e49fbc3
岸本陽大 <youodf11khp@gmail.com>
2019-10-31 15:43:24 +0900

    step2. load data

--- src/exp/exp4_simple/step1_set_global_variables.py
+++ src/exp/exp4_simple/step1_set_global_variables.py
@@ -6,7 +6,6 @@ PROJECT_DIR = os.getcwd()
 INPUT_FILE_PATH = Path("../..")
 sys.path.insert(0, PROJECT_DIR)
 
-from src.logger.main import LOGGER, setup_logger  # isort:skip # noqa: E402
 
 # settings
 EXP_ID = "exp4"
@@ -16,10 +15,3 @@ ID_COLUMNS = "id"
 TARGET_COLUMNS = ["target"]
 N_CLASSES = len(TARGET_COLUMNS)
 N_FOLDS = 5  # n_folds for Cross-Validation
-
-# logger set up
-LOGGER_PATH = "{}_log.txt".format(EXP_ID)
-if os.path.isfile(LOGGER_PATH):
-    os.remove(LOGGER_PATH)
-setup_logger(out_file=LOGGER_PATH)
-LOGGER.info("seed={}".format(SEED))
